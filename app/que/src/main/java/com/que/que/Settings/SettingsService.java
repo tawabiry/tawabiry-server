7b45537c39caf3b496c41851c6fb5c680f537648
FamALouiz <fam@awadlouis.com>
2025-03-19 22:05:09 +0100

    Updated warning response body to return scheduled maintenance time

--- app/que/src/main/java/com/que/que/Settings/SettingsService.java
+++ app/que/src/main/java/com/que/que/Settings/SettingsService.java
@@ -1,5 +1,6 @@
 package com.que.que.Settings;
 
+import java.util.Date;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ public class SettingsService {
     public static boolean isMaintenanceMode = false;
     public static boolean isComingSoonMode = false;
     public static boolean isWarning = false;
+    public static Date scheduledMaintenanceTime = null;
     private long maintenanceDuration = 10; // in minutes
 
     public Map<String, Object> getSettings() {
@@ -33,12 +35,15 @@ public class SettingsService {
                 public void run() {
                     SettingsService.isMaintenanceMode = true;
                     SettingsService.isWarning = false;
+                    SettingsService.scheduledMaintenanceTime = null;
                 }
             }, 60 * 60 * 1000);
             SettingsService.isWarning = true;
+            SettingsService.scheduledMaintenanceTime = new Date(System.currentTimeMillis() + 60 * 60 * 1000);
         } else {
             SettingsService.isMaintenanceMode = isMaintenanceMode;
             SettingsService.isWarning = false;
+            SettingsService.scheduledMaintenanceTime = null;
         }
 
         SettingsService.isComingSoonMode = isComingSoonMode;
@@ -55,6 +60,7 @@ public class SettingsService {
 
     public Map<String, Object> getWarning() {
         Map<String, Object> warning = Map.of("isWarning", SettingsService.isWarning);
+        warning.put("scheduledMaintenanceTime", SettingsService.scheduledMaintenanceTime);
         return warning;
     }
 }
